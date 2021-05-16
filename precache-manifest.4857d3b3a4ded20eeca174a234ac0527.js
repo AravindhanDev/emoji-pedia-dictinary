37f334f463cb7a7fbb4979b3143bc458fdd2dacd
noob <noobie37@protonmail.com>
2021-05-16 09:20:58 +0530

    Updates

---
+++ precache-manifest.4857d3b3a4ded20eeca174a234ac0527.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5fd1bdd946e9b509c38fb4ee5c907f",
+    "url": "/emoji-pedia-dictinary/index.html"
+  },
+  {
+    "revision": "d5a5629193d18619e834",
+    "url": "/emoji-pedia-dictinary/static/js/2.55870ca7.chunk.js"
+  },
+  {
+    "revision": "91bc0a2d459cff83e3f9",
+    "url": "/emoji-pedia-dictinary/static/js/main.cbff3824.chunk.js"
+  },
+  {
+    "revision": "1aa892d83bf1a32e84ae",
+    "url": "/emoji-pedia-dictinary/static/js/runtime-main.be540e72.js"
+  }
+]);
